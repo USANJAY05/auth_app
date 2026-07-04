a90616bc5e495e091cc2ab70b0862851eb5124b8
Sanjay Sanjay <sanjay.05@icloud.com>
2026-07-04 19:48:24 +0530

    handled error

--- src/core/init_db.py
+++ src/core/init_db.py
@@ -2,6 +2,9 @@ from src.core.conf import Base, engine
 from src.models.users import User
 
 def init_db():
-    Base.metadata.create_all(
-        bind=engine
-    )
+    try:
+        Base.metadata.create_all(
+            bind=engine
+        )
+    except Exception as e:
+        print("Failed to Connect to DB try to run the db and then start the app")
